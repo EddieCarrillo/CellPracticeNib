cd8f8d5004fe7466b57aa60c5215d3c9bf4bf29a
Eduardo Carrillo <edcarril@ucsd.edu>
2018-02-08 15:23:17 -0800

    Added text to custom cell created in XIB file.

--- CellPracticeNib/ViewController.swift
+++ CellPracticeNib/ViewController.swift
@@ -8,16 +8,49 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource {
+    
+    
+    let cellReuseId = "com.ecarrillo.tableviewcell"
+    
+    
+    var classes = ["CSE_105: Theory of Computation", "Math 184 Combinatorics", "Math 180A Probability"]
 
+    @IBOutlet weak var tableview: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        tableview.dataSource = self
+        //Load the nib
+        let cellNib = UINib(nibName: "TableViewCell", bundle: Bundle.main)
+        //Register the cell for use in the 
+        tableview.register(cellNib, forCellReuseIdentifier: cellReuseId)
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return classes.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellReuseId, for: indexPath) as! TableViewCell
+        
+        
+        let index = indexPath.row
+        
+        let currentClass = classes[index]
+        
+        cell.classText.text = currentClass
+        
+        
+        
+        return cell
     }
 
 
